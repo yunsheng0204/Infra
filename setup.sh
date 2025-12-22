abbb05fbac2a81d06f249b2667c724544d2f9641
yunsheng0204 <yunsheng1223@gmail.com>
2025-12-22 11:11:37 +0800

    Test

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# install virtualization tools
+sudo dnf -y update
+sudo dnf -y groupinstall "Virtualization Host"
+sudo systemctl enable --now libvirtd
+sudo dnf -y install virt-top libguestfs-tools
+lsmod | grep kvm
+
+ethernet_name=$(ls /sys/class/net | grep -E '^(enp|ens|eth)')
+
+# remove existing ethernet connection and set up a bridge network
+sudo nmcli connection show
+sudo nmcli connection delete "$ethernet_name"
+sudo nmcli connection add type bridge con-name br0 ifname br0
+sudo nmcli connection add type bridge-slave ifname "$ethernet_name" master br0
+
+sudo nmcli connection modify br0 ipv4.addresses 192.168.0.10/24
+sudo nmcli connection modify br0 ipv4.gateway 192.168.0.1
+sudo nmcli connection modify br0 ipv4.dns 192.168.0.1
+sudo nmcli connection modify br0 ipv4.method manual
